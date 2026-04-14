67205a47e35159ce149b43363d3d767bcdcca6ca
winsznx <timjosh507@gmail.com>
2026-04-14 14:01:45 +0100

    define RawPetState, LivePetState, and PetAction types

---
+++ packages/stxpet-core/src/types.ts
@@ -0,0 +1,27 @@
+export interface RawPetState {
+  hunger: number;
+  happiness: number;
+  energy: number;
+  lastInteractionBlock: number;
+  isAlive: boolean;
+  currentBlock: number;
+  totalRounds: number;
+}
+
+export interface LivePetState extends RawPetState {
+  effectiveHunger: number;
+  effectiveHappiness: number;
+  effectiveEnergy: number;
+  blocksUntilNextDecay: number;
+  isDangerZone: boolean;
+}
+
+export type PetAction = 'feed' | 'play' | 'sleep';
+
+export interface ContractCallOptions {
+  contractAddress: string;
+  contractName: string;
+  functionName: string;
+  functionArgs: readonly unknown[];
+  network: unknown;
+}
