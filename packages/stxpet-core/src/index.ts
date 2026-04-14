52a0d196c596fef0617fa1c7caf99b2aa4b9e6a7
winsznx <timjosh507@gmail.com>
2026-04-14 14:01:44 +0100

    add barrel exports for stxpet-core package

---
+++ packages/stxpet-core/src/index.ts
@@ -0,0 +1,14 @@
+export { parsePetState } from './parsers/parsePetState';
+export { computeCurrentMeters } from './parsers/computeCurrentMeters';
+export { parseDeathEvent } from './parsers/parseDeathEvent';
+export { buildFeedTx } from './transactions/buildFeedTx';
+export { buildPlayTx } from './transactions/buildPlayTx';
+export { buildSleepTx } from './transactions/buildSleepTx';
+
+export type {
+  RawPetState,
+  LivePetState,
+  PetAction,
+  ContractCallOptions,
+} from './types';
+export type { DeathEvent } from './parsers/parseDeathEvent';
