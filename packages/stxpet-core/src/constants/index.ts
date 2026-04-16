c67cc52d7ba8e1e2e55d9fbe9498c4da81c1eab4
winsznx <timjosh507@gmail.com>
2026-04-16 20:35:10 +0100

    feat(core): add barrel export for constants module

---
+++ packages/stxpet-core/src/constants/index.ts
@@ -0,0 +1,4 @@
+export { DECAY_CONFIG } from './decay';
+export type { DecayConfig } from './decay';
+export { CONTRACT_ERROR_CODE, contractErrorMessage } from './errorCodes';
+export type { ContractErrorCode } from './errorCodes';
