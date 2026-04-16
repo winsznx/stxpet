ce5280760442b030f71ff3de5f96c581bbe75470
winsznx <timjosh507@gmail.com>
2026-04-16 20:35:11 +0100

    feat(core): add barrel export for parsers module

---
+++ packages/stxpet-core/src/parsers/index.ts
@@ -0,0 +1,4 @@
+export { parsePetState } from './parsePetState';
+export { computeCurrentMeters } from './computeCurrentMeters';
+export { parseDeathEvent } from './parseDeathEvent';
+export type { DeathEvent } from './parseDeathEvent';
