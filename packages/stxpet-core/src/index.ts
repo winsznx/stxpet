ee1c03e78db330f8133b9ac0457dbafd11ed79ba
winsznx <timjosh507@gmail.com>
2026-04-16 20:35:10 +0100

    refactor(core): re-export constants and helpers from root barrel

--- packages/stxpet-core/src/index.ts
+++ packages/stxpet-core/src/index.ts
@@ -5,6 +5,11 @@ export { buildFeedTx } from './transactions/buildFeedTx';
 export { buildPlayTx } from './transactions/buildPlayTx';
 export { buildSleepTx } from './transactions/buildSleepTx';
 
+export { DECAY_CONFIG, CONTRACT_ERROR_CODE, contractErrorMessage } from './constants';
+export type { DecayConfig, ContractErrorCode } from './constants';
+
+export { isMeterCritical, getCurrentRoundNumber, minMeter, isPetDead } from './helpers';
+
 export type {
   RawPetState,
   LivePetState,
